9576e23f6dda1a89cc35275df297d0a581cae608
XuanBac-ww <ngoxuanbac11@gmail.com>
2026-04-12 13:28:13 +0700

    test: add unit coverage for auth and jwt services

--- SpringSecurity/src/test/java/com/example/SpringSecurity/SpringSecurityApplicationTests.java
+++ SpringSecurity/src/test/java/com/example/SpringSecurity/SpringSecurityApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.SpringSecurity;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SpringSecurityApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void applicationClassShouldExist() {
+		assertThat(SpringSecurityApplication.class).isNotNull();
 	}
 
 }
